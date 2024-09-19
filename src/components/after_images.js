7c316c6cba81e225afc6cb2cf55d40681f7baf2a
ShashwatDrashan <130088068+Shashwat-Darshan@users.noreply.github.com>
2024-09-19 15:34:15 +0530

    use this

--- src/components/after_images.js
+++ src/components/after_images.js
@@ -1,10 +1,11 @@
-
-import './after_images.css'; // Make sure to include your CSS file
+import './after_images.css';
 import React, { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import img1 from './images_for_upload_process_output/1.png';
-// Register the ScrollTrigger plugin
+import img2 from './images_for_upload_process_output/2.png';
+import img3 from './images_for_upload_process_output/3.png';
+
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
@@ -12,46 +13,39 @@ const App = () => {
 
   useEffect(() => {
     const container = containerRef.current;
-    const boxes = container.querySelectorAll('.box');
     const texts = container.querySelectorAll('.fade-text');
 
-    boxes.forEach((box, index) => {
-      // Box animation
-      gsap.to(box, {
-        x: 300,
-        scrollTrigger: {
-          trigger: box,
-          scrub: true,
-          start: 'top bottom',
-          end: 'bottom top'
-        }
-      });
-
-      // Text fade animation
-      gsap.to(texts[index], {
+    texts.forEach((text, index) => {
+      gsap.to(text, {
         opacity: 1,
-        duration: 0.5,
+        duration: 0.1,
         scrollTrigger: {
-          trigger: box,
+          trigger: text,
           start: 'top bottom',
           end: 'bottom top',
-          scrub: true,
           toggleActions: 'play reverse play reverse'
         }
       });
+
+      
     });
   }, []);
 
   return (
-    <div ref={containerRef}>
+    <div ref={containerRef} className="container">
       <h2>Scroll down</h2>
-      <div className="box"></div>
-      <div className="fade-text">Uploading the images to detect construction site</div>
-      <img src={img1} alt="this 1" />
-      <div className="box"></div>
-      <div className="fade-text">Model is analyzing images for which phase does the image</div>
-      <div className="box"></div>
-      <div className="fade-text">Output base on the multiple levels</div>
+      <div className="content-wrapper">
+        <div className="fade-text">Uploading the images to detect construction site</div>
+        <img src={img1} alt="Upload process" className="process-image" />
+      </div>
+      <div className="content-wrapper">
+        <div className="fade-text">Model is analyzing images for which phase does the image</div>
+        <img src={img2} alt="Analysis process" className="process-image" />
+      </div>
+      <div className="content-wrapper">
+        <div className="fade-text">Output based on the multiple levels</div>
+        <img src={img3} alt="Output process" className="process-image" />
+      </div>
     </div>
   );
 };
